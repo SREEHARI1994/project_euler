12370b6afa24187b89c86c89bfb72601d0fd26c6
SREEHARI P V <37414290+SREEHARI1994@users.noreply.github.com>
2019-09-18 17:03:37 +0530

    trying separate modulus function

--- fibanocci_words.py
+++ fibanocci_words.py
@@ -1,19 +1,38 @@
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
 def fib(container,la,lb,n):
     tl=la + 2*lb
     r=n%tl
+    #r=mod(str(n),tl)
     #print("r is"+str(r))
     if r==1:
-        print(str(container[0]))
+        print(container[0])
     else:
         sub=r-1
-        start=abs(n-sub)
+        start=n-sub
         l=len(container)
-        index=(abs(n-start))%l
-        print(str(container[index]))
+        dif=n-start
+        index=dif%l
+        print(container[index])
 
 
-
+def mod(num, a): 
+      
+    # Initialize result 
+    res = 0
+  
+    # One by one process all digits 
+    # of 'num' 
+    for i in range(0, len(num)): 
+        res = (res * 10 + int(num[i])) % a; 
+  
+    return res 
 
 q=int(input())
 for i in range(q):
@@ -23,6 +42,7 @@ for i in range(q):
     n=int(line[2])
     len_a=len(a)
     len_b=len(b)
-    container=list(a)+list(b)
+    container=list(a)+list(b)+list(b)
     #print(container)
     fib(container,len_a,len_b,n)
+    
